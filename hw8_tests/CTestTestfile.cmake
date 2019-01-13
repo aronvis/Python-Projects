56a38a5ae79004601c1e7fcf134fe8a0f5e2bea5
Aron Vischjager <vischjag@usc.edu>
2019-01-12 22:29:10 -0800

    Update

--- hw8_tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs104/hw-vischjag/hw8/hw8_tests
-# Build directory: /home/cs104/hw-vischjag/hw8/hw8_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(testing_utils)
-subdirs(trie_tests)
--- hw8_tests/testing_utils/kwsys/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs104/hw-vischjag/hw8/hw8_tests/testing_utils/kwsys
-# Build directory: /home/cs104/hw-vischjag/hw8/hw8_tests/testing_utils/kwsys
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
